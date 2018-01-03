cd49eb7376016c24890b687a76707412ead26ed2
Randy Ferrer <randfer@gmail.com>
2018-01-02 19:52:09 -0500

    initial commit, core express app

---
+++ app.js
@@ -0,0 +1,91 @@
+var fs = require('fs');
+var http = require('http');
+var path = require('path');
+var methods = require('methods');
+var express = require('express');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var cors = require('cors');
+var passport = require('passport');
+var errorhandler = require('errorhandler');
+var mongoose = require('mongoose');
+
+var isProduction = process.env.NODE_ENV === 'production';
+
+// Create a global app object
+var app = express();
+
+app.use(cors());
+
+// Normal express config defaults
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(require('method-override')());
+app.use(express.static(__dirname + '/public'));
+
+app.use(
+  session({
+    secret: 'conduit',
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+if (!isProduction) {
+  app.use(errorhandler());
+}
+
+if (isProduction) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect('mongodb://locahost/conduit');
+  mongoose.set('debug', true);
+}
+
+app.use(require('./routes'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (!isProduction) {
+  app.use(function(err, req, res, next) {
+    console.log(err.stack);
+
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err
+      }
+    });
+  });
+}
+
+// production error handler
+// no stacktracers leaked to the user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    errors: {
+      message: err.message,
+      error: {}
+    }
+  });
+});
+
+// finally, we start the server
+var server = app.listen(process.env.PORT || 3000, function() {
+  console.log('Listening on port ' + server.address().port);
+});
